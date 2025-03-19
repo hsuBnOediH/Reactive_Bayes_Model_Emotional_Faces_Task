49270529189f57db590237105614e70d5ed608d0
Carter Goldman <carter.goldman19@gmail.com>
2025-03-19 10:53:41 -0500

    set up scripts to run on cluster

--- emotional_faces_scripts/HGF_emotional_faces.jl
+++ emotional_faces_scripts/HGF_emotional_faces.jl
@@ -19,17 +19,63 @@ if get(ENV, "CLUSTER", "false") == "true"
     results_dir = get(ENV, "RESULTS","RESULTS_NOT_SET")
     root = "/media/labs/"
     formatted_time = ""
+    hyperparam_str = get(ENV, "HYPERPARAM_STR", "NOT SET")
+    println("Hyperparams String: ", hyperparam_str)
+    hyperparam_dict = Dict(
+        pair[1] => parse(Float64, pair[2]) for pair in 
+        (split(kv, "=") for kv in split(hyperparam_str, ","))
+    )
+    prior_kappa_mean = get(hyperparam_dict, "prior_kappa_mean", "NOT SET")
+    prior_kappa_variance = get(hyperparam_dict, "prior_kappa_variance", "NOT SET")
+    prior_omega_mean = get(hyperparam_dict, "prior_omega_mean", "NOT SET")
+    prior_omega_variance = get(hyperparam_dict, "prior_omega_variance", "NOT SET")
+    prior_beta_shape = get(hyperparam_dict, "prior_beta_shape", "NOT SET")
+    prior_beta_rate = get(hyperparam_dict, "prior_beta_rate", "NOT SET")
+    prior_z_mean = get(hyperparam_dict, "prior_z_mean", "NOT SET")
+    prior_z_variance = get(hyperparam_dict, "prior_z_variance", "NOT SET")
+    prior_x_mean = get(hyperparam_dict, "prior_x_mean", "NOT SET")
+    prior_x_variance = get(hyperparam_dict, "prior_x_variance", "NOT SET")
+    initial_z_mean = get(hyperparam_dict, "initial_z_mean", "NOT SET")
+    initial_z_variance = get(hyperparam_dict, "initial_z_variance", "NOT SET")
+    initial_kappa_mean = get(hyperparam_dict, "initial_kappa_mean", "NOT SET")
+    initial_kappa_variance = get(hyperparam_dict, "initial_kappa_variance", "NOT SET")
+    initial_omega_mean = get(hyperparam_dict, "initial_omega_mean", "NOT SET")
+    initial_omega_variance = get(hyperparam_dict, "initial_omega_variance", "NOT SET")
+    initial_beta_shape = get(hyperparam_dict, "initial_beta_shape", "NOT SET")
+    initial_beta_rate = get(hyperparam_dict, "initial_beta_rate", "NOT SET")
+    niterations = get(hyperparam_dict, "niterations", "NOT SET")
+    niterations = Int(niterations) # cast as an integer
 else
     println("Running locally...")
-    Pkg.activate("L:/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces/"); # Note that the Project and Manifest files are in the same directory as this script
-    subject_id = "55bcd160fdf99b1c4e4ae632"
-    predictions_or_responses = "responses"
+    Pkg.activate("L:/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces/"); # Note that the Project and Manifest files are in the same directory as this script.
+    # Click Julia: Activate this Environment to run the REPL
+    subject_id = "5c3c1617f5ebd500018596cb"
+    predictions_or_responses = "responses" # Haven't set up infrastructure to fit predictions
     results_dir = "L:/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/model_output_prolific/hgf_RxInfer_test"
     root = "L:/"
     # get current datetime
     using Dates
     now_time = now()
     formatted_time = "_" * Dates.format(now_time, "yyyy-mm-ddTHH_MM_SS")
+    prior_kappa_mean = 1.0
+    prior_kappa_variance = 0.1
+    prior_omega_mean = 0.0
+    prior_omega_variance = .01 # used to be .01
+    prior_beta_shape = 1.0
+    prior_beta_rate = 1.0
+    prior_z_mean = 0.0
+    prior_z_variance = 1.0
+    prior_x_mean = 0.0
+    prior_x_variance = 1.0
+    initial_z_mean = 0.0
+    initial_z_variance = 10.0
+    initial_kappa_mean = 1.0
+    initial_kappa_variance = 0.1
+    initial_omega_mean = 0.0
+    initial_omega_variance = 0.01
+    initial_beta_shape = 0.1
+    initial_beta_rate = 0.1
+    niterations = 20
 end
 Pkg.instantiate()  # Reinstall missing dependencies
 
@@ -40,6 +86,7 @@ using Distributions
 using Plots, StatsPlots
 using StableRNGs
 using CSV, DataFrames
+import ReactiveMP: as_companion_matrix, ar_transition, getvform, getorder, add_transition!
 
 
 # Seed for reproducibility
@@ -47,8 +94,7 @@ seed = 23
 rng = StableRNG(seed)
 
 # Read in the task data
-
-file_name = root * "rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/data/task_data_$(subject_id)_$(predictions_or_responses).csv"
+file_name = root * "rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces_processed_data/task_data_$(subject_id)_$(predictions_or_responses).csv"
 data = CSV.read(file_name, DataFrame)
 obs_data = data.observed
 obs_data = Float64.(obs_data)
@@ -60,13 +106,13 @@ resp_data = Float64.(resp_data)
 # todo: esitmate variance on top level (z_variance). could potentially fix the middle mean to .5; 
 @model function hgf_smoothing(obs, resp, z_variance)
     # Initial states - adjust means to be closer to data range
-    z_prev ~ Normal(mean = 0.0, variance = 1.0)  # Reduced variance
-    x_prev ~ Normal(mean = 0.0, variance = 1.0)  # Reduced variance
+    z_prev ~ Normal(mean = prior_z_mean, variance = prior_z_variance)  # Reduced variance
+    x_prev ~ Normal(mean = prior_x_mean, variance = prior_x_variance)  # Reduced variance
  
     # Priors on κ and ω - maybe adjust these
-    κ ~ Normal(mean = 1.0, variance = 0.1)  # Reduced variance and mean
-    ω ~ Normal(mean = 0.0, variance = 0.01)  # Reduced variance
-    β ~ Gamma(shape = 1.0, rate = 1.0)  # Less constrained
+    κ ~ Normal(mean = prior_kappa_mean, variance = prior_kappa_variance)  # Reduced variance and mean
+    ω ~ Normal(mean = prior_omega_mean, variance = prior_omega_variance)  # Reduced variance
+    β ~ Gamma(shape = prior_beta_shape, rate = prior_beta_rate)  # Less constrained
 
     for i in eachindex(obs)
         # Higher layer update (Gaussian random walk)
@@ -79,7 +125,6 @@ resp_data = Float64.(resp_data)
         obs[i] ~ Probit(x[i])
  
         # Noisy binary response (Bernoulli likelihood)
-    
         temp[i] ~ softdot(β, x[i], 1.0)
         resp[i] ~ Probit(temp[i])
  
@@ -96,7 +141,6 @@ resp_data = Float64.(resp_data)
     q(β) :: ProjectedTo(Gamma)
 end
 
-import ReactiveMP: as_companion_matrix, ar_transition, getvform, getorder, add_transition!
 
 @meta function hgfmeta_smoothing()
     # Lets use 31 approximation points in the Gauss Hermite cubature approximation method
@@ -152,16 +196,16 @@ end
 
 
 
-function run_inference_smoothing(obs, resp, z_variance, niterations=10)
+function run_inference_smoothing(obs, resp, z_variance, niterations)
     @initialization function hgf_init_smoothing()
-        q(z) = NormalMeanVariance(0, 10)
-        q(κ) = NormalMeanVariance(1.0, 0.1)
-        q(ω) = NormalMeanVariance(0.0, 0.01)
-        q(β) = GammaShapeRate(0.1, 0.1)
+        q(z) = NormalMeanVariance(initial_z_mean, initial_z_variance)
+        q(κ) = NormalMeanVariance(initial_kappa_mean, initial_kappa_variance)
+        q(ω) = NormalMeanVariance(initial_omega_mean, initial_omega_variance)
+        q(β) = GammaShapeRate(initial_beta_shape, initial_beta_rate)
     end
 
     return infer(
-        model = hgf_smoothing(z_variance = 0.1,),  # Reduced z_variance
+        model = hgf_smoothing(z_variance=z_variance,),  # Reduced z_variance
         data = (obs = obs, resp = resp,),
         meta = hgfmeta_smoothing(),
         constraints = hgfconstraints_smoothing(),
@@ -185,7 +229,7 @@ function run_inference_smoothing(obs, resp, z_variance, niterations=10)
     )
 end
 
-infer_result = run_inference_smoothing(obs_data, resp_data, 5.0, 20)
+infer_result = run_inference_smoothing(obs_data, resp_data, 1.0, niterations)
 
 # Extract posteriors
 x_posterior = infer_result.posteriors[:x]
@@ -224,7 +268,6 @@ savefig(joinpath(results_dir, "hidden_states_and_parameters_" * subject_id * for
 
 
 ## Investigate Accuracy of the Model
-
 # Extract free energy values
 free_energy_vals = infer_result.free_energy
 println("Free Energy values: ", free_energy_vals)
@@ -235,10 +278,9 @@ savefig(joinpath(results_dir, "free_energy_" * subject_id * formatted_time * ".p
 
 
 # Get probability assigned to response
-# not sure if I should use the mean or precision-weighted mean
+# For each temp_posterior, pull out the weighted mean (xi) and precision (w) values and calculate the probability of the response
 probit_prob = [cdf(Normal(0,1), t.xi / t.w) for t in temp_posterior]
-probit_prob = [cdf(Normal(0,1), t.xi ) for t in temp_posterior]
-
+# Calculate the probability that the model assigned to each response
 action_probability_values = [resp == 1 ? p : 1 - p for (resp, p) in zip(resp_data, probit_prob)]
 mean(action_probability_values)
 
@@ -257,9 +299,28 @@ results_df = DataFrame(
     beta_mean = mean(β_posterior),
     beta_std = std(β_posterior),
     free_energy_last = last(free_energy_vals),
-    mean_action_prob = mean(action_probability_values)
+    mean_action_prob = mean(action_probability_values),
+    prior_kappa_mean = prior_kappa_mean,
+    prior_kappa_variance = prior_kappa_variance,
+    prior_omega_mean = prior_omega_mean,
+    prior_omega_variance = prior_omega_variance,
+    prior_beta_shape = prior_beta_shape,
+    prior_beta_rate = prior_beta_rate,
+    prior_z_mean = prior_z_mean,
+    prior_z_variance = prior_z_variance,
+    prior_x_mean = prior_x_mean,
+    prior_x_variance = prior_x_variance,
+    initial_z_mean = initial_z_mean,
+    initial_z_variance = initial_z_variance,
+    initial_kappa_mean = initial_kappa_mean,
+    initial_kappa_variance = initial_kappa_variance,
+    initial_omega_mean = initial_omega_mean,
+    initial_omega_variance = initial_omega_variance,
+    initial_beta_shape = initial_beta_shape,
+    initial_beta_rate = initial_beta_rate,
+    niterations = niterations,
 )
 
-# Save the results as a CSV file
+# Save the results as a CSV 
 output_file = joinpath(results_dir, "model_results_" * subject_id * formatted_time * ".csv")
 CSV.write(output_file, results_df)
---
+++ emotional_faces_scripts/HGF_loop.py
@@ -0,0 +1,89 @@
+import sys, os, re, subprocess, neptune
+from datetime import datetime 
+current_datetime = datetime.now().strftime("%m-%d-%Y_%H-%M-%S")
+
+results = f"/media/labs/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/model_output_prolific/hgf_RxInfer_{current_datetime}"
+if not os.path.exists(results):
+    os.makedirs(results)
+    print(f"Created results directory {results}")
+
+if not os.path.exists(f"{results}/logs"):
+    os.makedirs(f"{results}/logs")
+    print(f"Created results-logs directory {results}/logs")
+
+
+ssub_path = '/media/labs/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces_scripts/run_HGF_rxinfer.ssub'
+
+stdout_name = f"{results}/logs/EF-%J.stdout"
+stderr_name = f"{results}/logs/EF-%J.stderr"
+
+predictions_or_responses = "responses"
+
+subject_list_path = '/media/labs/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/emotional_faces_prolific_IDs.csv'
+subjects = []
+with open(subject_list_path) as infile:
+    for line in infile:
+        subjects.append(line.strip())
+
+# Read in Neptune API Token
+with open('carter_neptune_token.txt', 'r') as file:
+    neptune_api_token = file.read().strip()
+
+# Initialize Neptune run to log hyperparameters and results
+run = neptune.init_run(
+    project="navid.faryad/Emotional-Faces-RxInfer",
+    api_token={neptune_api_token},
+)  
+run["batch/cluster"] = "true"
+run["batch/results_directory"] = results
+# Fetch the actual run ID so I can append results of fits to this run
+batch_run_id = run["sys/id"].fetch()  
+print(f"Batch Run ID: {batch_run_id}")
+
+# Define hyperparameters
+hyperparameters = {
+    "prior_kappa_mean" : 1.0,
+    "prior_kappa_variance" : 0.1,
+    "prior_omega_mean" : 0.0,
+    "prior_omega_variance" : .01, # used to be .01
+    "prior_beta_shape" : 1.0,
+    "prior_beta_rate" : 1.0,
+    "prior_z_mean" : 0.0,
+    "prior_z_variance" : 1.0,
+    "prior_x_mean" : 0.0,
+    "prior_x_variance" : 1.0,
+    "initial_z_mean" : 0.0,
+    "initial_z_variance" : 10.0,
+    "initial_kappa_mean" : 1.0,
+    "initial_kappa_variance" : 0.1,
+    "initial_omega_mean" : 0.0,
+    "initial_omega_variance" : 0.01,
+    "initial_beta_shape" : 0.1,
+    "initial_beta_rate" : 0.1,
+    "niterations" : 20,
+}
+# Log hyperparameters to Neptune
+for key, value in hyperparameters.items():
+    run[f"hyperparameters/{key}"] = value
+
+# Log source code in Neptune
+run["source_code/files"].upload_files([
+    "/media/labs/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces_scripts/HGF_loop.py",
+    "/media/labs/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces_scripts/run_HGF_rxinfer.ssub",
+    "/media/labs/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces_scripts/HGF_emotional_faces.jl"
+])
+# Create hyperaparameter string to pass into bash
+hyperparam_str = ",".join(f"{key}={value}" for key, value in hyperparameters.items())
+print(f"Hyperparams String: {hyperparam_str}")  # Debugging
+
+# Fit each subjects' data
+for subject in subjects:
+    jobname = f'EF-RxInfer-{subject}'
+
+    stdout_name = f"{results}/logs/{subject}-%J.stdout"
+    stderr_name = f"{results}/logs/{subject}-%J.stderr"
+    os.system(f"sbatch -J {jobname} -o {stdout_name} -e {stderr_name} {ssub_path} {results} {subject} {predictions_or_responses} {batch_run_id} {hyperparam_str}")
+
+    print(f"SUBMITTED JOB [{jobname}]")
+
+#python3 /media/labs/rsmith/lab-members/cgoldman/Wellbeing/emotional_faces/RxInfer_scripts/emotional_faces/HGF_loop.py
